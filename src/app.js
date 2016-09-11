381f74e7da58ee2b790b17cf25ead64913e4387c
Meathanjay Baidya <baidya.me@gmail.com>
2016-09-12 02:49:39 +0600

    todoService

--- src/app.js
+++ src/app.js
@@ -1,11 +1,43 @@
 angular.module('todoApp', [])
-  .controller('mainCtrl', function($scope) {
-    $scope.todos = [
-      { "name": "Clean Housing" },
-      { "name": "Water the Flowers" },
-      { "name": "Feed the Dog" },
-      { "name": "Pay diim bills" },
-      { "name": "Run" },
-      { "name": "Swim" }
-    ]
+  .controller('mainCtrl', function($scope, todoService) {
+
+    $scope.addTodo = function() {
+      var todo = {"name" : "Added a new todo!"}
+      $scope.todos.push(todo);
+    };
+
+    todoService.getTodos(function(res) {
+      $scope.todos = res.data;
+    });
+
+    $scope.deleteTodo = function(todo, index) {
+      todoService.deleteTodo(todo);
+      $scope.todos.splice(index,1);
+    }
+
+    $scope.saveTodo = function(todo) {
+      todoService.saveTodo(todo);
+    }
+
+  })
+  .service('todoService', function($http) {
+
+    this.helloConsole = function() {
+      console.log("This is hello from Service!");
+    };
+
+    this.getTodos = function(callback){
+      $http.get('/api/mock/todos.json').then(callback)
+    };
+
+    this.deleteTodo = function(todo) {
+      console.log('The ' + todo.name + " was successfully deleted!");
+      // send a delete request to server to actually delete the todo
+    };
+
+    this.saveTodo = function(todo) {
+      console.log(todo.name + " is updated!");
+      // send a put request to server to actually save the todo
+    };
+
   });
